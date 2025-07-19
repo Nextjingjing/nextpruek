5c5db8b2216cae8058bd1f39745d6904de890f49
SNeXT <snext2004@gmail.com>
2025-07-19 10:44:12 +0700

    fix bug prisma cannot run npm run prisma:dev in alpine22

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return '!hello'; // Test pass
+    return '!hello '; // Test pass
   }
 }
 
